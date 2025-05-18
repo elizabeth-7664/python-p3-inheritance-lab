955b74f40111f47ae038f94ee52251b30573d824
elizabeth ndinda <elizabethndinda41@gmail.com>
2025-05-18 18:26:29 +0300

    defining methods in the child class that use the super keyword to inherit from and augment methods in the parent class

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,10 @@
-#!/usr/bin/env python
 
 from user import User
 
-class Student(User):
-    
-    def learn(self):
-        pass
+class Student(User):  # 👈 Inherit from User
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)  # 👈 Use super to set names
+        self.knowledge = []  # 👈 Students start with no knowledge
+
+    def learn(self, string):
+        self.knowledge.append(string)  # 👈 Add what the student learns
--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,23 @@
-#!/usr/bin/env python
-
-from user import User
-
+# lib/teacher.py
 import random
+from user import User  # Make sure to import User class
 
-class Teacher(User):
+knowledge = [
+    "str is a data type in Python",
+    "programming is hard, but it's worth it",
+    "JavaScript async web request",
+    "Python function call definition",
+    "object-oriented teacher instance",
+    "programming computers hacking learning terminal",
+    "pipenv install pipenv shell",
+    "pytest -x flag to fail fast",
+]
+
+class Teacher(User):  # 👈 Inherit from User
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)  # 👈 Call the parent’s __init__
+        self.knowledge = knowledge  # 👈 Set up the knowledge list
 
     def teach(self):
-        pass
+        index = random.randint(0, len(self.knowledge) - 1)
+        return self.knowledge[index]
